c0135071d0ef8fa02e8cf90d593afdad55a26697
Guilherme Santos <guisantossilva00@gmail.com>
2022-09-23 14:36:34 -0300

    Site Finalizado

--- js/index.js
+++ js/index.js
@@ -2,3 +2,14 @@ const toggle = document.getElementById('toggle');
 toggle.onclick = function(){
     toggle.classList.toggle('active');
 }
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-total").style.top = "0px";
+  } else {
+    document.getElementById("nav-total").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
